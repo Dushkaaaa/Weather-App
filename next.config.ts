e166251ba25bf9d5bf61324c8589864d0bf4ade8
Dushkaaaa <dushkevychyevhenii@gmail.com>
2026-01-18 21:47:52 +0100

    add package tailwindcss

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = "Weather-App";
-
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-  images: {
-    unoptimized: true,
-  },
+
   reactCompiler: true,
 };
 
